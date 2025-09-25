34b12e32187c478393091256f420c4b51e993d4e
IsaacRojasBarrón <isaac.robarron@gmail.com>
2025-09-25 14:17:16 -0600

    Actualización eea.c

--- P3/eea.c
+++ P3/eea.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 typedef struct {
     int gcd;
@@ -15,6 +17,11 @@ Resultado eea(int r0, int r1) {
     int ti_2 = t0, ti_1 = t1;
 
     int ri, qi, si, ti;
+    int iter = 1;
+
+    printf("\nIteraciones del algoritmo extendido de Euclides:\n");
+    printf("Iteracion| q   | r        | s        | t\n");
+    printf("-------------------------------------------\n");
 
     do {
         ri = ri_2 % ri_1;
@@ -23,10 +30,13 @@ Resultado eea(int r0, int r1) {
         si = si_2 - qi * si_1;
         ti = ti_2 - qi * ti_1;
 
-    
+        printf("%5d    | %2d  | %8d | %8d | %8d\n", iter, qi, ri, si, ti);
+
         ri_2 = ri_1; ri_1 = ri;
         si_2 = si_1; si_1 = si;
         ti_2 = ti_1; ti_1 = ti;
+
+        iter++;
 
     } while (ri != 0);
 
@@ -35,10 +45,18 @@ Resultado eea(int r0, int r1) {
 }
 
 int main() {
-    int r0 = 9, r1 = 11;
-    Resultado r = eea(r0, r1);
-    printf("gcd(%d,%d) = %d\n", r0, r1, r.gcd);
-    printf("s = %d, t = %d\n", r.s, r.t);
+    srand(time(NULL));
+
+    for (int i = 0; i < 5; i++) {
+        int r0 = rand() % 100 + 1;
+        int r1 = rand() % 100 + 1;
+        
+        Resultado r = eea(r0, r1);
+        printf("\nResultado final:\n");
+        printf("gcd(%d,%d) = %d\n", r0, r1, r.gcd);
+        printf("s = %d, t = %d\n", r.s, r.t);
+    }
 
     return 0;
 }
+
